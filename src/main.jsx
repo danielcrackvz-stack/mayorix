638673170a4e2ad1a84bce2b5ebf335e952febff
danielcrackvz <danielcrackvz@gmail.com>
2026-09-15 12:53:10 -0400

    Corrige rutas de GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename="/mayorix">
       <AuthProvider>
         <App />
       </AuthProvider>
